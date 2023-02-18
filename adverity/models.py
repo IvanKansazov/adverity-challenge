ae59104ab968ccb7c913cd9c5df23c3cc4ed49d5
Ivan Kansazov <ivan.kansazov@smartwitness.com>
2023-02-18 10:15:13 +0200

    Implement mysql orm
    Django ORM will read from .cnf file
    .cnf file need to be completed from .cnf.example

---
+++ adverity/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Collections(models.Model):
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=255)
+    created = models.DateTimeField('date created')
